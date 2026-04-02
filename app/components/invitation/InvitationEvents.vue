6ff90ade25742fa39bb94877f7536c1b1b7043a6
Wildan Rizky <wildanrizkiyadi68@gmail.com>
2026-04-03 06:35:13 +0700

    change date

--- app/components/invitation/InvitationEvents.vue
+++ app/components/invitation/InvitationEvents.vue
@@ -3,7 +3,7 @@
     <InvitationFlowers />
     <h3 class="inv-events__title">Akad Nikah</h3>
     <p class="inv-events__date">Selasa, 7 April 2026</p>
-    <p class="inv-events__time">16.00 WIB s/d Selesai</p>
+    <p class="inv-events__time">18.30 WIB s/d Selesai</p>
     <p class="inv-events__place">Kediaman Mempelai Wanita<br/>
     Ds. Tembeleng RT 13/02 Jatibarang - Brebes</p>
     <a
